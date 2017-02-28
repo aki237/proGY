1f9234c62833a033625c7b24a0a2019064305831
Akilan Elango <akilan1997@gmail.com>
2017-03-01 03:50:22 +0530

    Added a control unix socket to reload configuration on the fly  * Restructured the config to Connect to multiple proxy servers on the  go.

---
+++ control.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func listenUnixControl(fileChannel chan string) {
+	socketName := "/tmp/proGY-control"
+	if _, err := os.Stat(socketName); err == nil {
+		if os.Remove(socketName) != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+	ln, err := net.ListenUnix("unix", &net.UnixAddr{socketName, "unix"})
+	if err != nil {
+		log("%s\n", err)
+		os.Exit(2)
+	}
+	for {
+		conn, _ := ln.Accept()
+		go func(conn net.Conn) {
+			b := bufio.NewReader(conn)
+			s, e := b.ReadString('\n')
+			if e != nil {
+				return
+			}
+			s = strings.TrimSpace(s)
+			splitted := strings.Split(s, " ")
+			if len(splitted) < 2 {
+				conn.Close()
+				return
+			}
+			if splitted[0] != "RELOAD" {
+				conn.Close()
+				return
+			}
+			filename := ""
+			for i, val := range splitted[1:] {
+				filename += val
+				if i != len(splitted[1:])-1 {
+					filename += " "
+				}
+			}
+			fileChannel <- filename
+			conn.Write([]byte("SUCCESS\n"))
+			conn.Close()
+			return
+		}(conn)
+	}
+}
